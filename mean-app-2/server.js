4b63d0c114cc130411a1737ee6af2262adf56389
Christopher Mamian <christophermamian@gmail.com>
2015-07-14 10:29:22 -0700

    Get all users

--- mean-app-2/server.js
+++ mean-app-2/server.js
@@ -87,4 +87,14 @@ apiRouter.route('/users')
 		});
 	})
 
+	// get all the users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+		User.find(function(err, users) {
+			if (err) res.send(err);
+
+			// return the users
+			res.json(users);
+		})
+	})
+
 app.use('/api', apiRouter);
